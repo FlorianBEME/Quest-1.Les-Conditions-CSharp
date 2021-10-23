31fdea412e2869c837a9144bf14d130d1701587f
FlorianBEME <69846887+FlorianBEME@users.noreply.github.com>
2021-10-23 12:54:36 +0200

    Ajoutez des fichiers projet.

---
+++ Quest 1.Les Conditions CSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Quest_1.Les_Conditions_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min;
+            int max;
+            int Integer;
+            Console.WriteLine("Enter Minimum");
+            min = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Max");
+            max = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Integer");
+            Integer = Int32.Parse(Console.ReadLine());
+
+        }
+    }
+}
